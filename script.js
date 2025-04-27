420137e6b00f097a61ec98de3a4bec40e3a7f5e2
D. Ajay Kumar <154305661+ajay70aa@users.noreply.github.com>
2025-04-27 12:41:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const textDisplay = document.getElementById('status')
+const enterBtn = document.getElementById('enterBtn')
+
+function handleClick (){
+	textDisplay.innerHTML = '<h1>Entered Metaverse</h1>'
+}
+
+enterBtn.addEventListener('click', handleClick)
